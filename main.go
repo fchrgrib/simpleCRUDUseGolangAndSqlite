ef05a134b2c7cb0ec121d19825d102c7c48b6c37
fchrgrib <fahrian.alifudin@gmail.com>
2023-01-17 06:47:40 +0700

    Update

--- main.go
+++ main.go
@@ -12,7 +12,19 @@ func main() {
 
 	r := gin.Default()
 	r.GET("/users", models.GetUsersDetail)
-	r.GET("/users/:name", models.GetUsers)
+	//r.GET("/users/:name", models.GetUsers)
+
+	r.GET("/users/:id", models.CreateUser)
+	r.GET("/users/location/:coordinate-Location", models.CreateLocation)
+	r.GET("/users/school/:coordinate-School", models.CreateSchool)
+
+	r.PATCH("/users/:id", models.UpdateUsers)
+	r.PATCH("/users/location/:coordinate-Location", models.UpdateLocation)
+	r.PATCH("/users/school/:coordinate-School", models.UpdateSchool)
+
+	r.DELETE("/users/:id", models.DeleteUsers)
+	r.DELETE("/users/location/:coordinate-Location", models.DeleteLocation)
+	r.DELETE("/users/school/:coordinate-School", models.DeleteSchool)
 
 	if err := r.Run("localhost:7788"); err != nil {
 		panic(err)
